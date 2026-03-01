55d8a2a997a58b19727137676682e1e32f62439e
Rieko Sakai <nekoyomi@icloud.com>
2026-03-01 10:11:59 +0900

    Initial Commit

---
+++ EdoTime/EdoTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdoTimeApp.swift
+//  EdoTime
+//
+//  Created by RiekoSakai on 2026/03/01.
+//
+
+import SwiftUI
+
+@main
+struct EdoTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
